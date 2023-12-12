071b96827301d6c68524bfc7753d52cebd9c207e
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2023-12-13 00:37:39 +0200

    Remove node_modules from repository

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_18_163330) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_12_223047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "collections", force: :cascade do |t|
+    t.datetime "time"
+    t.string "note_from_kiki"
+    t.string "message_from_alfred"
+    t.string "bags"
+    t.bigint "subscription_id", null: false
+    t.boolean "is_done", default: false, null: false
+    t.boolean "skip", default: false, null: false
+    t.boolean "needs_bags", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subscription_id"], name: "index_collections_on_subscription_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "collection_day"
+    t.integer "plan"
+    t.date "start_date"
+    t.integer "duration"
+    t.string "subburb"
+    t.boolean "is_paused", default: false, null: false
+    t.bigint "user_id", null: false
+    t.string "customer_id"
+    t.date "holiday_start"
+    t.date "holiday_end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "role"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "collections", "subscriptions"
+  add_foreign_key "subscriptions", "users"
 end
